053b579e65d89f202df3ce3ce1076f3a04e40e3d
Thomas CARTON <carton.t@sfeir.com>
2019-09-22 20:17:11 +0200

    Create initial project

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CustomPicker
+//
+//  Created by Thomas CARTON
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
